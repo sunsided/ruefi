b9ff6449eedf3bf929833ca056c509a85c2f2063
Markus Mayer <widemeadows@gmail.com>
2025-10-20 21:05:16 +0200

    Use expect instead of unwrap consistently

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,16 @@
 All notable changes to this project will be documented in this file.
 This project uses [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+[Unreleased]: https://github.com/sunsided/uefi-experiments/compare/v0.1.0..HEAD
+
+## Internal
+
+- Make error handling explicit and more consistent.
+
 ## [0.1.0] - 2025-10-20
 
-<!-- [Unreleased]: https://github.com/sunsided/uefi-experiments/compare/277456229a7d7ab07a82e250b514cb2683432a0c..HEAD -->
 [0.1.0]: https://github.com/sunsided/uefi-experiments/releases/tag/v0.1.0
 
 ### Added
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@
 #![allow(unsafe_code)]
 
 use uefi::prelude::*;
+use uefi::println;
 use uefi::proto::console::text::Color;
 
 #[entry]
 fn main() -> Status {
-    uefi::helpers::init().unwrap();
+    uefi::helpers::init().expect("failed to initialize UEFI");
+
+    println!("Hello, world from Rust!");
 
     system::with_stdout(|stdout| {
-        stdout.reset(false).unwrap();
-        stdout.set_color(Color::LightRed, Color::Black).unwrap();
-        stdout.output_string(cstr16!("Hello, world from bare-metal UEFI (Rust)!\r\n")).unwrap();
+        stdout.reset(false).expect("failed to write to reset stdout");
+        stdout.set_color(Color::LightRed, Color::Black).expect("failed to write to set colors");
+        stdout.output_string(cstr16!("Hello, world from bare-metal UEFI (Rust)!\r\n")).expect("failed to write to stdout");
     });
 
     wait_for_key();
@@ -22,28 +25,28 @@ fn main() -> Status {
 
 fn wait_for_key() {
     system::with_stdout(|out| {
-        let _ = out.output_string(cstr16!("Press any key or wait 5s…\r\n"));
+        out.output_string(cstr16!("Press any key or wait 5s…\r\n")).expect("failed to write to stdout");
     });
 
     // timer event for 5 seconds (units are 100 ns)
-    let timer = unsafe { boot::create_event(boot::EventType::TIMER, boot::Tpl::APPLICATION, None, None).unwrap() };
-    boot::set_timer(&timer, boot::TimerTrigger::Relative(5 * 10_000_000)).unwrap();
+    let timer = unsafe { boot::create_event(boot::EventType::TIMER, boot::Tpl::APPLICATION, None, None).expect("failed to create timer event") };
+    boot::set_timer(&timer, boot::TimerTrigger::Relative(5 * 10_000_000)).expect("failed to set timer");
 
     // key event
-    let key_ev = system::with_stdin(|stdin| stdin.wait_for_key_event().unwrap());
+    let key_ev = system::with_stdin(|stdin| stdin.wait_for_key_event().expect("failed to wait for key event"));
 
     // wait for either event
     let mut events = [key_ev, timer];
-    let idx = boot::wait_for_event(&mut events).unwrap();
+    let idx = boot::wait_for_event(&mut events).expect("failed to wait for event");
     match idx {
         0 => {
             // index 0: key event fired first
-            let _ = system::with_stdin(|stdin| stdin.read_key());
+            system::with_stdin(|stdin| stdin.read_key()).expect("failed to read key");
         }
         1 => {
             // index 1: timer fired first
             system::with_stdout(|out| {
-                let _ = out.output_string(cstr16!("\r\nTimeout reached. Goodbye!\r\n"));
+                out.output_string(cstr16!("\r\nTimeout reached. Goodbye!\r\n")).expect("failed to write to stdout");
             });
         }
         _ => unreachable!(),
